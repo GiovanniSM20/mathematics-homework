4969fef949d774eb345faf3ac473c035bf4e51b9
Martins Giovanni <giovannimartins2000@gmail.com>
2018-05-09 01:00:15 -0300

    removing Gulpfile.js

--- Gulpfile.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp'),
-  childProcess = require('child_process'),
-  electron = require('electron-prebuilt');
-  
-gulp.task('run', function () {
-  childProcess.spawn(electron, ['./app'], { stdio: 'inherit' });
-});
