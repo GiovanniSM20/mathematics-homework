64146af0e31ac737dcfe404cf68cfc96b514f9f4
Martins Giovanni <giovannimartins2000@gmail.com>
2018-05-09 00:51:46 -0300

    now it's gone

---
+++ Gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp'),
+  childProcess = require('child_process'),
+  electron = require('electron-prebuilt');
+  
+gulp.task('run', function () {
+  childProcess.spawn(electron, ['./app'], { stdio: 'inherit' });
+});
